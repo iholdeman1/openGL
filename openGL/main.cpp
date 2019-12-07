a82ccd478a79f16638f71fcbb3728df60b9ee79e
iholdeman1 <ian.holdeman0510@gmail.com>
2019-12-07 18:04:55 -0500

    Initial Commit

---
+++ openGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL
+//
+//  Created by Ian Holdeman on 12/7/19.
+//  Copyright © 2019 Marvin LLC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
